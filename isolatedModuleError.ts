370b6b4c37189b86eb89f20095a30bd007a39f6f
Brent Erickson <brent2737@gmail.com>
2018-03-10 16:32:11 -0800

    Remove test case

--- isolatedModuleError.ts
+++ isolatedModuleError.ts
@@ -1,5 +1,7 @@
+/*
+// Typescript teams believes this is by design https://github.com/Microsoft/TypeScript/issues/20703
 import tsutils = require('tsutils');
-
+*/
 export class Test {
     noop() {
     }
