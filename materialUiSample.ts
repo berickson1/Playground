e08e930dcdd452e9fe877bbb29b1ac5937b125b9
Brent Erickson <berickson1@users.noreply.github.com>
2020-10-16 14:30:55 -0700

    Distill example down a bit

--- materialUiSample.ts
+++ materialUiSample.ts
@@ -1,27 +1,29 @@
-import { createStyles, makeStyles } from '@material-ui/core';
+import { createStyles, makeStyles, Theme, TypographyStyle } from '@material-ui/core';
 
-const useStylesBroken = makeStyles(theme =>
-    createStyles({
-        sizeSmall: {
-            ...theme.typography.button, // This is simply a set of css styles - nothing relating to props
-            height: '36px',
-        },
-    })
+const fakeTheme: Theme = 4 as any;
+const fakeTypographyStyle: TypographyStyle = 4 as any;
+
+const brokenStyle = createStyles({
+    sizeSmall: {
+        ...fakeTypographyStyle, // This is simply a set of css styles - nothing relating to props
+        height: '36px',
+    },
+})
+const workingStyle = createStyles({
+    sizeSmall: {
+        height: '36px',
+    },
+})
+const workingStyle2 = createStyles({
+    sizeSmall: {
+        ...fakeTypographyStyle,
+    },
+})
+const useStylesBroken = makeStyles(() => brokenStyle
 );
-const useStylesWorks = makeStyles(theme =>
-    createStyles({
-        sizeSmall: {
-            height: '36px',
-        },
-    })
+const useStylesWorks = makeStyles(() => workingStyle
 );
-const useStylesWorks2 = makeStyles(theme =>
-    createStyles({
-        sizeSmall: {
-            ...theme.typography.button,
-        },
-    })
-);
+const useStylesWorks2 = makeStyles(() => workingStyle2);
 
 export function NotReallyAButton(): null {
     console.log(useStylesBroken());
