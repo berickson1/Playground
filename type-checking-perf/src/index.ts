d6f1574c90bb7ec2e6ac84c840540bcd91b44537
Brent Erickson <berickson1@users.noreply.github.com>
2021-11-18 09:30:09 -0800

    Add minimal repro

---
+++ type-checking-perf/src/index.ts
@@ -0,0 +1,13 @@
+/* Start block to comment out */
+import yup from 'yup'
+
+export function testFunction(): void {
+    const yupString = yup.string()
+    console.log(yupString);
+}
+
+/* End block to comment out */
+
+export function log(): void {
+    console.log('test');
+}
